f1897b00fc67fc0655cb7f73f249c872d4158910
沛粤 杨 <1005612968@qq.com>
2023-06-01 02:08:21 +0800

    实现游客访问

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -14,22 +14,23 @@ func AuthMiddleware() gin.HandlerFunc {
 		//获取authorization header
 		tokenString := ctx.GetHeader("Authorization")
 		//若token为空或token格式不正确
-		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足: 100}"})
-			ctx.Abort()
-			return
-		}
+		if tokenString == "" || len(tokenString) <= 7 || !strings.HasPrefix(tokenString, "Bearer ") {
+            // 如果请求头中没有Authorization信息，或者信息不合法，则视为游客访问。
+            ctx.Set("user", nil)
+            ctx.Next()
+            return
+        }
 		tokenString = tokenString[7:]
 		token, claims, err := common.ParseToken(tokenString)
 		if err != nil || !token.Valid {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足: 200}}"})
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足}"})
 			ctx.Abort()
 			return
 		}
 		//验证通过后获取claims中的userId
 		userId := claims.UserID
 		if userId == 0 {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足:301"})
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
 			ctx.Abort()
 			return
 		}
@@ -38,12 +39,12 @@ func AuthMiddleware() gin.HandlerFunc {
 		db.Where("userID = ?", userId).First(&user)
 		//用户不存在
 		if user.UserID == 0 {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足:302"})
-			ctx.Abort()
-			return
+			// 如果用户不存在，则将user设置为nil，表示当前用户为游客。
+			ctx.Set("user", nil)
+		} else {
+			// 用户存在，将user的信息写入上下文
+			ctx.Set("user", user)
 		}
-		//用户存在，将user的信息写入上下文
-		ctx.Set("user", user)
 		ctx.Next()
 	}
 }
