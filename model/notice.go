f48f20bdde9d4b2f9736d02eccaf6eebd6766009
thinkerhui <1142200122@qq.com>
2023-06-03 23:17:19 +0800

    完善了通知页面，现在可以接受帖子评论、评论的回复、惩罚、反馈、举报处理的通知，并且对于帖子评论、评论的回复的通知点击通知响应位置会自动跳转到相应的位置(返回按钮也搞好了）。后端通知发送的逻辑错误也修复了，现在收到评论的回复有两种情况：一是其他人在我的帖子的评论下回复，二是其他人回复了我的二级评论。这两种情况不可同时发生，或者说同时发生也只收到一条通知。 在侧边栏显示未读通知数量，数量为0则不显示，每次跳转路由的时候都会计算一次通知的数量。主页帖子显示头像和图片，帖子详情页也显示作者头像。由于举报和反馈还要基于其他人的一些工作，所有还没完善，但显示的框架已经搭建好了

--- model/notice.go
+++ model/notice.go
@@ -7,7 +7,7 @@ type Notice struct {
 	NoticeID int       `gorm:"primary_key;column:noticeID"`
 	Receiver int       `gorm:"index:noticereceiver;column:receiver;type:int;not null"`
 	User     User      `gorm:"association_foreignkey:receiver;foreignkey:userID"`
-	Sender   int       `gorm:"index:noticesender;column:sender;type:int;not null"`
+	Sender   int       `gorm:"index:noticesender;column:sender;type:int"`
 	Type     string    `gorm:"column:type;type:enum('pcomment','ccomment','punish','feedback','sue')"`
 	Ntext    string    `gorm:"column:ntext;type:varchar(1000)"`
 	Time     time.Time `gorm:"column:time;type:datetime"`
