72d8c225e460945eaccf0908771b5f3bf83166a8
沛粤 杨 <1005612968@qq.com>
2023-08-21 21:20:21 +0800

    更新api

--- api/api.go
+++ api/api.go
@@ -4,12 +4,13 @@ package api
 import (
 	"encoding/base64"
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
 	tms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tms/v20201229"
-        "net/http"
 )
 
 func GetSuggestion(inStr string) string {
@@ -17,10 +18,10 @@ func GetSuggestion(inStr string) string {
 	// 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
 	// 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
 	credential := common.NewCredential(
-		"AKIDxyvhQ2SeacXEfz9qLenkSvFNI3kJtT2R",
-		"KSVNfmiU3To7LwLenkUGuZ1OehWCkMn1",
+		"AKIDDKI6jEct34uZ3Nm4CAzeAVfdWG0dHrIc",
+		"GJBD5lDYkjV5WmsoYHqFmAMNFxUp01mb",
 	)
-	
+
 	// 实例化一个client选项，可选的，没有特殊需求可以跳过
 	cpf := profile.NewClientProfile()
 	cpf.HttpProfile.Endpoint = "tms.tencentcloudapi.com"
@@ -49,5 +50,5 @@ func ApiTest(c *gin.Context) {
 	}
 	c.Bind(&inData)
 	suggestion := GetSuggestion(inData.InputVal)
-    c.JSON(http.StatusOK, gin.H{"outputVal": suggestion})
+	c.JSON(http.StatusOK, gin.H{"outputVal": suggestion})
 }
